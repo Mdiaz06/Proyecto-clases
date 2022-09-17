f77915ace0a8b1adacf520d982bf326396c0c5f1
Mdiaz06 <110851594+Mdiaz06@users.noreply.github.com>
2022-09-16 22:12:52 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,10 +1,10 @@
 public class Estudiante{
     private String nombre;
     private String apellido;
-    private int idEstudiante;
+    private String idEstudiante;
     private String carrera;
   
-    public Estudiante(String nombre, String apellido, int idEstudiante, String carrera){
+    public Estudiante(String nombre, String apellido, String idEstudiante, String carrera){
       this.nombre = nombre;
       this.apellido = apellido;
       this.idEstudiante = idEstudiante;
