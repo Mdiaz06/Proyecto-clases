bcb5d4fa7f2a7a273a8c66bcbbeebb2f6d2ac063
Mdiaz06 <110851594+Mdiaz06@users.noreply.github.com>
2022-09-16 22:17:33 -0500

    Update and rename MateriaAlumno.java to MateriaEstudiante.java

---
+++ MateriaEstudiante.java
@@ -0,0 +1,11 @@
+public class MateriaEstudiante{
+    private int codMateria;
+    private String idEstudiante;
+    
+  
+    public MateriaAlumno(int codMateria, String idEstudiante){
+      this.codMateria = codMateria;
+      this.idEstudiante = idEstudiante;
+    }
+    
+  }
